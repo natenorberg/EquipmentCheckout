74e99af730542b4476792dc1a3fafa2a2934d771
nate.norberg <natenorberg@gmail.com>
2013-12-16 18:38:30 -0700

    Setup: renamed database

--- CheckoutSystem/urls.py
+++ CheckoutSystem/urls.py
@@ -5,8 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'CheckoutSystem.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    #url(r'^$', 'CheckoutSystem.views.home', name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
