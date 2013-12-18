6db3bc08560120d3fbd7f26069bb731ad3f3e283
nate.norberg <natenorberg@gmail.com>
2013-12-18 15:14:51 -0700

    6: Created ability to log in to the page

--- CheckoutSystem/urls.py
+++ CheckoutSystem/urls.py
@@ -7,6 +7,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
+                       url(r'^welcome/$', views.index),
+                       url(r'^progress/$', views.progress),
+                       url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+                       url(r'^accounts/logout/$', views.logout_view),
                        url(r'^checkout/', include('checkout.urls')),
                        url(r'^admin/', include(admin.site.urls)),
 )
--- CheckoutSystem/views.py
+++ CheckoutSystem/views.py
@@ -1,5 +1,19 @@
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 
 def index(request):
-    return render_to_response("welcome.html")
+    return render_to_response("welcome.html", context_instance=RequestContext(request))
+
+
+@login_required
+def progress(request):
+    return render_to_response("progress.html", context_instance=RequestContext(request))
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect("/welcome/")
