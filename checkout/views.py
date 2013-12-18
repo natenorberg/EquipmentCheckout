76faf61b8b82f10de0e0b3066f565cecb471f536
nate.norberg <natenorberg@gmail.com>
2013-12-17 20:12:28 -0700

    25: Gave welcome page its own html file

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 
 def index(request):
-    return HttpResponse('Website under maintenance.')
+    return render_to_response("checkout/index.html")
