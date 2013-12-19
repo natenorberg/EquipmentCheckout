12bb049694c147f68341b36baa3f9e90b034b2e8
brianmaher <bmaher91@gmail.com>
2013-12-19 13:04:28 -0800

    #15 (Doing) Added some basic equipment CRUD functionality

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from checkout.models import Equipment
 
-# Register your models here.
+
+class EquipmentAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        (None, {'fields': ['quantity']}),
+        ('Date Information', {'fields': ['date_added'], 'classes': ['collapse']})
+    ]
+    list_display = ('name', 'date_added', 'quantity')
+    list_filter = ['date_added']
+    search_fields = ['name']
+
+
+admin.site.register(Equipment, EquipmentAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField('date published')
+    quantity = models.IntegerField(max_length=200)
+
+    def __str__(self):
+        return self.name
