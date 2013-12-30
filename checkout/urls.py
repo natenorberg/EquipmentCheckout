6e86f772a40b32f1d0e3727d297b89480dcb2222
nate.norberg <natenorberg@gmail.com>
2013-12-30 16:46:12 -0700

    16: Created view for list of all equipment

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
 
 from checkout import views
+from checkout.views import EquipmentListView
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
+                       url(r'equipment/$', EquipmentListView.as_view())
 )
--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render_to_response
+from django.views.generic import ListView
+from checkout.models import Equipment
 
 
 def index(request):
     return render_to_response("checkout/index.html")
+
+
+class EquipmentListView(ListView):
+    model = Equipment
