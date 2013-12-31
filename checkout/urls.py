9d7684363846d7e45ea0581fb52d37a814d595ee
nate.norberg <natenorberg@gmail.com>
2013-12-30 19:12:46 -0700

    16: Created view for equipment details

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 
 from checkout import views
-from checkout.views import EquipmentListView
+from checkout.views import EquipmentListView, equipment_detail
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
-                       url(r'equipment/$', EquipmentListView.as_view())
+                       url(r'equipment/$', EquipmentListView.as_view()),
+                       url(r'equipment/(?P<equipment_id>\d+)/$', equipment_detail)
 )
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.views.generic import ListView
 from checkout.models import Equipment
 
@@ -9,3 +9,8 @@ def index(request):
 
 class EquipmentListView(ListView):
     model = Equipment
+
+
+def equipment_detail(request, equipment_id=None):
+    equipment = get_object_or_404(Equipment, pk=equipment_id)
+    return render_to_response("checkout/equipment_detail.html", {"equipment": equipment})
